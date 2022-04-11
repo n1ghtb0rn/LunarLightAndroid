c40948c2836e36da3a433e7ea88366c8abf26e7b
n1ghtb0rn <daniel.falkedal@gmail.com>
2022-04-11 23:38:26 +0200

    comment spelling error

--- app/src/main/java/com/danielfalkedal/lunarlight/Factories/WorldMessageViewModelFactory.kt
+++ app/src/main/java/com/danielfalkedal/lunarlight/Factories/WorldMessageViewModelFactory.kt
@@ -6,7 +6,7 @@ import com.danielfalkedal.lunarlight.Collections.WorldMessageModel
 import com.danielfalkedal.lunarlight.ViewModels.WorldMessagesViewModel
 import java.lang.IllegalStateException
 
-//Factory class to prevent ViewModel from being re-instanciated every time the View updates its state
+//Factory class to prevent ViewModel from being re-instantiated every time the View updates its state
 class WorldMessageViewModelFactory(private val worldMessageModel: WorldMessageModel) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
