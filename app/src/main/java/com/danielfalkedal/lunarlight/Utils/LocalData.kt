82edf7a2fbadfd22a7e3df924fbe044d23615f93
Daniel Falkedal <daniel.falkedal@gmail.com>
2022-04-14 09:11:58 +0200

    added stone images dictonary (map)

--- app/src/main/java/com/danielfalkedal/lunarlight/Utils/LocalData.kt
+++ app/src/main/java/com/danielfalkedal/lunarlight/Utils/LocalData.kt
@@ -14,7 +14,115 @@ object LocalData {
 
     val profileBackground = arrayOf("Capricorn", "Aquarius", "Pisces","Aries", "Taurus", "Gemeni", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius")
 
+    val stoneImages = mapOf(
 
+        "Capricorn" to arrayOf(
+            "capricorn_1",
+            "capricorn_2",
+            "capricorn_3",
+            "capricorn_4",
+            "capricorn_5",
+            "capricorn_6"
+        ),
+
+        "Aquarius" to arrayOf(
+            "aquarius_1",
+            "aquarius_2",
+            "aquarius_3",
+            "aquarius_4",
+            "aquarius_5",
+            "aquarius_6"
+        ),
+
+        "Pisces" to arrayOf(
+            "pisces_1",
+            "pisces_2",
+            "pisces_3",
+            "pisces_4",
+            "pisces_5",
+            "pisces_6"
+        ),
+
+        "Aries" to arrayOf(
+            "aries_1",
+            "aries_2",
+            "aries_3",
+            "aries_4",
+            "aries_5"
+        ),
+
+        "Taurus" to arrayOf(
+            "taurus_1",
+            "taurus_2",
+            "taurus_3",
+            "taurus_4",
+            "taurus_5",
+            "taurus_6",
+            "taurus_7"
+        ),
+
+        "Gemeni" to arrayOf(
+            "gemeni_1",
+            "gemeni_2",
+            "gemeni_3",
+            "gemeni_4",
+            "gemeni_5"
+        ),
+
+        "Cancer" to arrayOf(
+            "cancer_1",
+            "cancer_2",
+            "cancer_3",
+            "cancer_4",
+            "cancer_5"
+        ),
+
+        "Leo" to arrayOf(
+            "leo_1",
+            "leo_2",
+            "leo_3",
+            "leo_4",
+            "leo_5",
+            "leo_6",
+            "leo_7"
+        ),
+
+        "Virgo" to arrayOf(
+            "virgo_1",
+            "virgo_2",
+            "virgo_3",
+            "virgo_4",
+            "virgo_5"
+        ),
+
+        "Libra" to arrayOf(
+            "libra_1",
+            "libra_2",
+            "libra_3",
+            "libra_4",
+            "libra_5",
+            "libra_6",
+            "libra_7"
+        ),
+
+        "Scorpio" to arrayOf(
+            "scorpio_1",
+            "scorpio_2",
+            "scorpio_3",
+            "scorpio_4",
+            "scorpio_5",
+            "scorpio_6",
+            "scorpio_7"
+        ),
+
+        "Sagittarius" to arrayOf(
+            "sagittarius_1",
+            "sagittarius_2",
+            "sagittarius_3",
+            "sagittarius_4",
+            "sagittarius_5"
+        )
+    )
 
 
 }
