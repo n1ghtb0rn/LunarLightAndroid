1c6d2a680e0bbc17c4f11cabd61945f6bd6cdf2d
Daniel Falkedal <daniel.falkedal@gmail.com>
2022-04-15 13:06:53 +0200

    implemented user profile info

--- app/src/main/java/com/danielfalkedal/lunarlight/MainViews/ProfileView.kt
+++ app/src/main/java/com/danielfalkedal/lunarlight/MainViews/ProfileView.kt
@@ -6,7 +6,11 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -15,16 +19,24 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.danielfalkedal.lunarlight.AppIndexManager
 import com.danielfalkedal.lunarlight.Firebase.Repos.Models.Friend
 import com.danielfalkedal.lunarlight.Firebase.Repos.Models.User
+import com.danielfalkedal.lunarlight.Firebase.Repos.UserDao
+import com.danielfalkedal.lunarlight.Realm.Repos.RealmUserDao
 import com.danielfalkedal.lunarlight.ui.theme.getUserBackgroundColor
 
 
 @Composable
 fun ProfileView(user: User) {
+
+    val infoIndex = remember { mutableStateOf(0) }
+
+    val stoneInfo = "A great stone!"
+    val profileInfo = remember { mutableStateOf(user.profile_info)}
 
     Column(
         modifier = Modifier
@@ -37,7 +49,7 @@ fun ProfileView(user: User) {
                     )
                 )
             )
-            .fillMaxWidth(),
+            .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
@@ -66,16 +78,51 @@ fun ProfileView(user: User) {
         }
 
         Row() {
-            Button(onClick = { /*TODO*/ }) {
+            Button(onClick = {
+                infoIndex.value = 0
+            }) {
                 Text("About me")
             }
 
-            Button(onClick = { /*TODO*/ }) {
+            Button(onClick = {
+                infoIndex.value = 1
+            }) {
                 Text("Stone name")
             }
         }
 
-        Spacer(Modifier.fillMaxHeight())
+        if (infoIndex.value == 0) {
+            if (user.id == AppIndexManager.loggedInUser.id) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    TextField(
+                        value = profileInfo.value,
+                        onValueChange = {
+                            profileInfo.value = it
+                        },
+                    )
+                    Button(onClick = {
+                        user.profile_info = profileInfo.value
+
+                        AppIndexManager.loggedInUser.profile_info = profileInfo.value
+                        UserDao().createOrUpdateUser(AppIndexManager.loggedInUser)
+
+                        AppIndexManager.userRealm.profile_info = profileInfo.value
+                        RealmUserDao().updateUser(AppIndexManager.userRealm)
+                    }) {
+                        Text("Save")
+                    }
+                }
+            }
+            else {
+                Text(profileInfo.value)
+            }
+        }
+        else {
+            Text(stoneInfo)
+        }
+
 
     }
 
